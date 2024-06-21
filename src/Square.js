2c2412d9aef3ed658b01b3fa0b613d47cc359632
jigz-paisen <mfifox32@gmail.com>
2024-06-21 01:36:23 +0100

    Basic Tictactoe game tbh

---
+++ src/Square.js
@@ -0,0 +1,12 @@
+// src/Square.js
+import React from 'react';
+
+function Square(props) {
+  return (
+    <button className="square" onClick={props.onClick}>
+      {props.value}
+    </button>
+  );
+}
+
+export default Square;
